6104d1557a1d2b37c8ded72d2d6c4e55888391c7
Christoph Schimpf <christoph@r-schimpf.de>
2016-05-29 22:16:06 +0200

    test

---
+++ Chat/Code/Database.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat
+{
+    class Database
+    {
+        Dictionary<string, string> UserDatabase = new Dictionary<string, string>();
+        
+        public string registration(string Username, string Password)
+        {
+            try
+            {
+                UserDatabase.Add(Username, Password);
+
+                return "Die Registrierung war erfolgreich!";
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }            
+        }
+        
+        public void login(string Username, string Password)
+        {
+            if(UserDatabase.ContainsKey(Username))
+            {
+                //if(UserDatabase.)
+            }
+        } 
+    }
+}
